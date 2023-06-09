dedbd0502fe0b0460e7ed593014bafa281507637
Emma Carr <lorenagracex@gmail.com>
2023-06-09 09:22:24 -0500

    lol finishing up this finally

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,1 @@
-$(document).ready(function () {
-  // function that prevents just empty page on load
- let currentDay = dayjs().format('MMM DD, YYYY')
-//  console.log(currentDay); <- logged the current day
 
-// function to have default city load 
-function defaultC() {
-  cityName = "Austin"
-  findCity();
-}
-defaultC();
-
-let cityHistory = JSON.parse(localStorage)
-
-
-
-});
